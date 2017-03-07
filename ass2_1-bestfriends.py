e513e2381abd5a5cd007ce48248d1d5e3ad3b52a
Janca <jamafyna@gmail.com>
2017-03-07 15:57:41 +0100

    Dodelana druha cast prvni casti, neotestovano.

--- ass2_1-bestfriends.py
+++ ass2_1-bestfriends.py
@@ -10,41 +10,68 @@ sin=sys.stdin
 sout=sys.stdout 
 
 
+DIST=5 # the distance between 2 words
+LOW=1 # the number for disregarding: if word appears less than LOW times  
 
 
 
-
-def getPMI(a,b):
-        if (a,b) not in Pij: 
+def getPMI(a,b,P_ij,P_i):
+        if (a,b) not in P_ij: 
             print("debug 0")
             return 0
-        else: return math.log(Pij[a,b]/(Pi[a]*Pi[b]))
+        else: return math.log(P_ij[a,b]/(P_i[a]*P_i[b]))
         
 
 f=open(sys.argv[1],encoding="iso-8859-2",mode='rt')
 wordsdata=f.read().splitlines() # given words
 wordsuniq=col.Counter(wordsdata)
-w10=col.Counter({w:wordsuniq[w] for w in wordsuniq if wordsuniq[w]>9})
+wn10=col.Counter({w:wordsuniq[w] for w in wordsuniq if wordsuniq[w]<LOW})
 uniqbigrams=col.Counter(zip(wordsdata[:-1],wordsdata[1:])) # bigrams only from given words, not added start and end boundaries
 wordcount=len(wordsdata)
 bigramcount=wordcount-1
 Pi={w : wordsuniq[w]/wordcount for w in wordsuniq} # Pi ... probability P(i)
 Pij={u : uniqbigrams[u]/bigramcount for u in uniqbigrams} # Pij ... joint probability  P(i,j), j is immediately after the word i
 
-I=col.Counter({(u,v):math.log(Pij[u,v]/(Pi[u]*Pi[v])) for (u,v) in uniqbigrams if u in w10 and v in w10})
+I=col.Counter({(u,v):math.log(Pij[u,v]/(Pi[u]*Pi[v])) for (u,v) in uniqbigrams if u not in wn10 and v not in wn10})
 
 print(I.most_common(20))
 print(I.most_common(-20)) #nevyžaduje, ale je fajn
 
-#------------po sem to funguje
+#------------po sem to funguje, odtud neni overene
 
-#distance max 50 - MUSÍ SE POČÍTAT JINÝM ZPŮSOBEM, NAPŘ NEVYUŽÍVAT P(A,B) JAKO PST PO SOBĚ JDOUCÍCH SLOV, ALE NAPŘ PST, ŽE JE MEZI SLOVY DISTANCE MAX 50 - ZEPTAT SE
+
 Idis=col.Counter()
+distant=col.Counter()
 for i in range (0,wordcount-1):
-    for j in range (1,50):
-        if(i+j<wordcount):
+    for j in range (1,DIST):
+        if(i-j>=0):
+            u=wordsdata[i-j]
             w=wordsdata[i]
+            distant[u,w]+=1
+            #if(w in w10 and u in w10):
+            #    Idis[u,w]=getPMI(w,u)
+totalcount=sum(distant.values())
+Pdij={pair : distant[pair]/totalcount for pair in distant} # probability of distant words
+Idis=col.Counter({(u,v):getPMI(u,v,Pdij,Pi) for (u,v) in distant if u not in wn10 and v not in  wn10})
+
+#----2.varianta, pro to, kdy beru oba směry explicitně
+
+# z hlediska psti a dal je jedno, zda pouzivame toto nebo vyse, protože v tomto bude každé slovo 2x, ale i 2x věší počet slov, tedy stejné výsledky
+I2dis=col.Counter()
+distant2=col.Counter()
+for i in range (0,wordcount-1):
+    for j in range (1,DIST):
+        if(i-j>=0):
+            u=wordsdata[i-j]
+            w=wordsdata[i]
+            distant2[u,w]+=1
+        if(i+j<wordcount-1):
             u=wordsdata[i+j]
-            if(w in w10 and u in w10):
-                Idis[u,w]=getPMI(w,u)
+            w=wordsdata[i]
+            distant2[w,u]+=1
+            #if(w in w10 and u in w10):
+            #    Idis[u,w]=getPMI(w,u)
+totalcount2=sum(distant2.values())
+Pdij2={pair : distant2[pair]/totalcount2 for pair in distant2} # probability of distant words
+Idis2=col.Counter({(u,v):getPMI(u,v,Pdij2,Pi) for (u,v) in distant2 if u not in wn10 and v not in  wn10})
 
